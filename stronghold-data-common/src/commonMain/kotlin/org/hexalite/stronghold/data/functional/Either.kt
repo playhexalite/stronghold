ba7801301fed58013685ce76e33721db6fcafdd2
eexsty <hello@exst.fun>
2022-08-22 09:39:47 -0300

    feat: add either extensions and more utility functions

--- stronghold-data-common/src/commonMain/kotlin/org/hexalite/stronghold/data/functional/Either.kt
+++ stronghold-data-common/src/commonMain/kotlin/org/hexalite/stronghold/data/functional/Either.kt
@@ -6,19 +6,21 @@ import kotlinx.serialization.descriptors.SerialKind
 import kotlinx.serialization.descriptors.buildSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import org.hexalite.stronghold.data.functional.Either.Companion.either
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
 /**
- * A wrapper for a value that may be two distinct types. It is inspired by the functional
- * programming paradigm, and it is used to avoid the usage of exceptions on Hexalite to allow
- * a more developer-friendly environment.
+ * A wrapper for a value that may be two distinct types. It is inspired by the functional programming
+ * paradigm, and it is used to avoid the usage of exceptions on Hexalite to allow a more developer-friendly
+ * environment.
  *
  * You can build an [Either] type by using one of the functions on companion:
- * * [Either.left]   to create an [Either] type bound at left.
- * * [Either.right]  to create an [Either] type bound at right.
+ * * [Either.left]   to create an [Either] type bound to the left.
+ * * [Either.right]  to create an [Either] type bound to the right.
  * * [Either.decide] to create an [Either] type based on the given value and generics.
+ * * [Either.either] to create an [Either] type bound to the left or right (falling back) by an extension function.
  *
  * An [Either] type is pure, declarative and immutable. You can create another [Either]
  * types from a single one by using map functions:
@@ -50,69 +52,73 @@ data class Either<L, R> private constructor(private var left: L? = null, private
     fun isRight() = right != null
 
     /**
-     * Returns the value bound at the left type ([L]) or null if it is not bound
-     * at the given position.
+     * Returns the value bound at the left type ([L]) or null if it is not bound at the given position.
      */
     fun leftOrNull() = left
 
     /**
-     * Returns the value bound at the right type ([R]) or null if it is not bound
-     * at the given position.
+     * Returns the value bound at the right type ([R]) or null if it is not bound at the given position.
      */
     fun rightOrNull() = right
 
     /**
-     * Returns the value bound at the left type ([L]) or throws a
-     * [BoundTypeMismatchException] if the value is not bound at the given position.
+     * Returns the value bound at the left type ([L]) or throws a [BoundTypeMismatchException] if the value
+     * is not bound at the given position.
      */
-    fun left() = left ?: throw BoundTypeMismatchException('L')
+    fun left() = leftOrThrow { BoundTypeMismatchException('L') }
 
     /**
-     * Returns the value bound at the right type ([R]) or throws a
-     * [BoundTypeMismatchException] if the value is not bound at the given position.
+     * Returns the value bound at the right type ([R]) or throws a [BoundTypeMismatchException] if the value
+     * is not bound at the given position.
      */
-    fun right() = right ?: throw BoundTypeMismatchException('R')
+    fun right() = rightOrThrow { BoundTypeMismatchException('R') }
 
     /**
-     * Returns the value bound at the left type ([L]) or throws the exception returned
-     * by the given [error] function. This function is highly inspired by Rust's
-     * `Result#expect`.
+     * Returns the value bound at the left type ([L]) or throws the exception returned by the given [error]
+     * function. This function is highly inspired by Rust's `Result#expect`.
+     * @param error
      */
+    @Dsl
     fun leftOrThrow(error: () -> Throwable) = leftOrNull() ?: throw error()
 
     /**
-     * Returns the value bound at the right type ([R]) or throws the exception returned
-     * by the given [error] function. This function is highly inspired by Rust's
-     * `Result#expect`.
+     * Returns the value bound at the right type ([R]) or throws the exception returned by the given [error]
+     * function. This function is highly inspired by Rust's `Result#expect`.
+     * @param error
      */
+    @Dsl
     fun rightOrThrow(error: () -> Throwable) = rightOrNull() ?: throw error()
 
     /**
-     * Returns the value bound at the left type ([L]) or returns the default value
-     * returned by the given [default] function.
+     * Returns the value bound at the left type ([L]) or returns the default value returned by the given
+     * [default] function.
+     * @param default
      */
+    @Dsl
     fun leftOrDefault(default: () -> L) = leftOrNull() ?: default()
 
     /**
-     * Returns the value bound at the right type ([R]) or returns the default value
-     * returned by the given [default] function.
+     * Returns the value bound at the right type ([R]) or returns the default value returned by the given
+     * [default] function.
+     * @param default
      */
+    @Dsl
     fun rightOrDefault(default: () -> R) = rightOrNull() ?: default()
 
     /**
      * Returns a Kotlin's [Result] representation from this [Either] binding.
-     * The [right] side is often referred as the "error" or "failure" type in functional
-     * languages or multiparadigm languages such as Rust.
+     * The [right] side is often referred as the "error" or "failure" type in functional languages or
+     * multi-paradigm languages such as Rust.
      */
     fun asResult(): Result<L> {
         return Result.success(leftOrNull() ?: return Result.failure(BoundTypeMismatchException('R')))
     }
 
     /**
-     * Apply the given [transform] function to the element bound on the [left] side
-     * ([L]) if it is present.
+     * Apply the given [transform] function to the element bound on the [left] side ([L]) if it is present.
      * @return A new [Either] type containing the result of this [mapLeft] call.
      */
+    @Dsl
     @OptIn(ExperimentalContracts::class)
     fun <T> mapLeft(transform: (L) -> T): Either<T, R> {
         contract {
@@ -122,10 +128,10 @@ data class Either<L, R> private constructor(private var left: L? = null, private
     }
 
     /**
-     * Apply the given [transform] function to the element bound on the [right] side
-     * ([R]) if it is present.
+     * Apply the given [transform] function to the element bound on the [right] side ([R]) if it is present.
      * @return A new [Either] type containing the result of this [mapRight] call.
      */
+    @Dsl
     @OptIn(ExperimentalContracts::class)
     fun <T> mapRight(transform: (R) -> T): Either<L, T> {
         contract {
@@ -134,6 +140,33 @@ data class Either<L, R> private constructor(private var left: L? = null, private
         return right(transform(right ?: return left(left ?: error("Both Either sides are bound to null."))))
     }
 
+    /**
+     * Executes the given [callback] if this [Either] type is bound to the [left] side ([L]).
+     * This is similar to 'fire-and-forget'.
+     * @param callback
+     */
+    @Dsl
+    inline fun ifLeft(callback: (L) -> Unit): Either<L, R> = also {
+        callback(leftOrNull() ?: return@also)
+    }
+
+    /**
+     * Executes the given [callback] if this [Either] type is bound to the [right] side ([R]).
+     * This is similar to 'fire-and-forget'.
+     * @param callback
+     */
+    @Dsl
+    inline fun ifRight(callback: (R) -> Unit): Either<L, R> = also {
+        callback(rightOrNull() ?: return@also)
+    }
+
+    /**
+     * Returns this same [Either] but with the bound types swapped. In other words:
+     *   [L] -> [R]
+     *   [R] -> [L]
+     */
+    fun swap(): Either<R, L> = Either(right, left)
+
     // overridden types
     override fun toString(): String = (left ?: right).toString()
 
@@ -141,26 +174,30 @@ data class Either<L, R> private constructor(private var left: L? = null, private
         /**
          * Creates an [Either] type bound on the [left] side ([L]) with the given [value] and with
          * an empty [right] side ([R]).
+         * @param value
          */
         fun <L, R> left(value: L): Either<L, R> = Either(left = value)
 
         /**
          * Creates an [Either] type bound to the [right] side ([R]) with the given [value] and with
          * an empty [left] side ([L]).
+         * @param value
          */
         fun <L, R> right(value: R): Either<L, R> = Either(right = value)
 
         /**
          * Creates an [Either] type based on callback exception handling where the success output is
          * bound to the [left] side ([L]) and the error type is bound to the [right] side ([R]).
+         * @param callback
          */
+        @Dsl
         @OptIn(ExperimentalContracts::class)
-        inline fun <L, reified R : Throwable> catching(block: () -> L): Either<L, R> {
+        inline fun <L, reified R : Throwable> catching(callback: () -> L): Either<L, R> {
             contract {
-                callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+                callsInPlace(callback, InvocationKind.EXACTLY_ONCE)
             }
             return try {
-                left(block())
+                left(callback())
             } catch (exception: Throwable) {
                 if (exception is R) {
                     right(exception)
@@ -173,22 +210,39 @@ data class Either<L, R> private constructor(private var left: L? = null, private
         /**
          * Creates an [Either] type bound at what side the given [value]'s type is.
          * @throws IllegalArgumentException if the given [value] does not match [L] and [R].
+         * @param value
          */
-        inline fun <reified L, reified R> decide(value: Any): Either<L, R> = if (value is L)
-            left(value)
-        else if (value is R)
-            right(value)
-        else
-            throw IllegalArgumentException(
+        inline fun <reified L, reified R> decide(value: Any): Either<L, R> = when (value) {
+            is L -> left(value)
+            is R -> right(value)
+            else -> throw IllegalArgumentException(
                 "The provided value for Either#decide is not either '${L::class.simpleName}' or '${R::class.simpleName}'."
             )
+        }
+
+        /**
+         * Creates an [Either] type bound to the [left] side ([L]) if the extension receiver is not null,
+         * otherwise [fallback] to an [Either] type bound to the [right] side ([R]).
+         * @param fallback
+         */
+        @Dsl
+        inline fun <reified L, reified R> L?.either(fallback: () -> R & Any): Either<L, R> {
+            if (this != null) {
+                return left(this)
+            }
+            return right(fallback())
+        }
     }
+
+    @DslMarker
+    @Target(AnnotationTarget.FUNCTION)
+    private annotation class Dsl
 
     @OptIn(InternalSerializationApi::class, ExperimentalSerializationApi::class)
     class Serializer<L, R>(private val leftSerializer: KSerializer<L>, private val rightSerializer: KSerializer<R>) :
         KSerializer<Either<L, R>> {
         override val descriptor: SerialDescriptor =
-            buildSerialDescriptor("org.hexalite.network.common.util.Either", SerialKind.CONTEXTUAL)
+            buildSerialDescriptor("org.hexalite.stronghold.data.functional.Either", SerialKind.CONTEXTUAL)
 
         override fun serialize(encoder: Encoder, value: Either<L, R>) {
             if (value.isLeft()) {
